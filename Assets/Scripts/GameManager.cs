4824c8cbdf295d8eb9d55adb2e6ed77e3ad9b410
Ross Holder <ross@apprefactory.ca>
2020-07-10 22:21:38 -0400

    StartCoroutine() was placed in the wrong default unity method (Update() instead of Start())

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,18 +6,18 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
 
-    private float spawnRate = 1f;
+    private float spawnRate = 1.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(SpawnTarget());
+        
     }
 
     IEnumerator SpawnTarget()
